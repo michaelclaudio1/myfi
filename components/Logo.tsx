8afad967e8765ab43f320536ad6b8d3da004cad5
michaelclaudio1 <122314464+michaelclaudio1@users.noreply.github.com>
2025-02-06 14:26:48 -0500

    reverting code to when vercel links worked

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,7 +5,7 @@ import React from "react";
 // When base page ({vercel-link}.com/) is created, switch these to href="/"
 function Logo() {
   return (
-    <Link href="/dashboard" className="flex items-center gap-2">
+    <Link href="/" className="flex items-center gap-2">
       <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />
       <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading tight tracking-tighter text-transparent">
         MyFi
@@ -16,7 +16,7 @@ function Logo() {
 
 export function LogoMobile() {
   return (
-    <Link href="/dashboard" className="flex items-center gap-2">
+    <Link href="/" className="flex items-center gap-2">
       <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading tight tracking-tighter text-transparent">
         MyFi
       </p>
